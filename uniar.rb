6093b55f7ec42cc878f033106d9dd7cd737dc873
litencatt <litencatt@gmail.com>
2022-12-04 17:44:17 +0900

    Brew formula update for uniar version v0.8.2

--- uniar.rb
+++ uniar.rb
@@ -5,22 +5,22 @@
 class Uniar < Formula
   desc "uniar is UNI'S ON AIR music and scene cards database and manage your scene cards collection tool."
   homepage "https://github.com/litencatt/uniar"
-  version "0.8.1"
+  version "0.8.2"
   license "MIT"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/litencatt/uniar/releases/download/v0.8.1/uniar_v0.8.1_darwin_arm64.zip"
-      sha256 "5bb7532a9c2b8e3081537d47f02e6384cd0aeaa3bd85a91e1cbfb89c97ef5b10"
+      url "https://github.com/litencatt/uniar/releases/download/v0.8.2/uniar_v0.8.2_darwin_arm64.zip"
+      sha256 "382b9ef337de594143d5d23cc0552a75e95bc2f184cd8f78a70d12e70c209423"
 
       def install
         bin.install "uniar"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/litencatt/uniar/releases/download/v0.8.1/uniar_v0.8.1_darwin_amd64.zip"
-      sha256 "711bbda8563e495f01e7c407efcb9b4b226f29190ab0ee020f262d023aa9ed6b"
+      url "https://github.com/litencatt/uniar/releases/download/v0.8.2/uniar_v0.8.2_darwin_amd64.zip"
+      sha256 "1d235988f847556ef89b0d18fa92b6f2dec2bf5c9cdf83797b4a8266c76eed1e"
 
       def install
         bin.install "uniar"
